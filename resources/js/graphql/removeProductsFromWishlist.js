e22ea9691108d1ecea8c0090450c31c21f7f9fcc
Bob Wezelman <bob@justbetter.nl>
2022-08-26 16:08:26 +0200

    Added ids to the queries (#4)

--- resources/js/graphql/removeProductsFromWishlist.js
+++ resources/js/graphql/removeProductsFromWishlist.js
@@ -15,6 +15,7 @@ export default {
                         id
                         product {
                             sku
+                            id
                         }
                     }
                 }
