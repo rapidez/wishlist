aff6db9cb7f5091bdce0d67f57664b4040cc317d
Bob <bob@justbetter.nl>
2022-09-01 08:41:04 +0200

    Added id to the overlooked query

--- resources/js/wishlist.js
+++ resources/js/wishlist.js
@@ -3,7 +3,7 @@ Vue.component('wishlist', require('Vendor/rapidez/wishlist/resources/js/Wishlist
 document.addEventListener('turbolinks:load', () => {
     window.app.$on('logged-in', async () => {
         let response = await axios.post(config.magento_url + '/graphql', {
-            query: '{ customer { wishlists { id items_v2 { items { id product { sku } } } } } }'
+            query: '{ customer { wishlists { id items_v2 { items { id product { sku, id } } } } } }'
         }, { headers: { Authorization: `Bearer ${localStorage.token}` }})
 
         localStorage.wishlist = JSON.stringify(response.data.data.customer.wishlists[0])
