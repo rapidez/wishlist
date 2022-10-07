862295090a5955e9688c3c938f6990697b2d3be8
Claudio Ferraro <54107647+jbclaudio@users.noreply.github.com>
2022-10-07 10:31:15 +0200

    Store header (#7)

--- resources/js/wishlist.js
+++ resources/js/wishlist.js
@@ -4,7 +4,7 @@ document.addEventListener('turbolinks:load', () => {
     window.app.$on('logged-in', async () => {
         let response = await axios.post(config.magento_url + '/graphql', {
             query: '{ customer { wishlists { id items_v2 { items { id product { sku id } } } } } }'
-        }, { headers: { Authorization: `Bearer ${localStorage.token}` }})
+        }, { headers: { Authorization: `Bearer ${localStorage.token}`, Store: config.store_code }})
 
         localStorage.wishlist = JSON.stringify(response.data.data.customer.wishlists[0])
         window.app.$emit('wishlist-changed')
