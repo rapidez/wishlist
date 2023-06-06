43719433b6ec29b94788387eba121decc7b56374
Roene <roene@justbetter.nl>
2023-06-06 09:23:38 +0200

    Changed buttons

--- resources/views/wishlist/button.blade.php
+++ resources/views/wishlist/button.blade.php
@@ -6,15 +6,15 @@
             :callback="wishlistCallback"
         >
             <form slot-scope="{ mutate, mutated }" v-on:submit.prevent="mutate">
-                <x-rapidez::button type="submit">
+                <x-rapidez::button.primary type="submit">
                     <x-heroicon-s-heart v-if="isOnWishlist" class="h-5 w-5" />
                     <x-heroicon-o-heart v-else class="h-5 w-5" />
-                </x-rapidez::button>
+                </x-rapidez::button.primary>
             <form>
         </graphql-mutation>
     </div>
 </wishlist>
 
-<x-rapidez::button v-else href="/login">
+<x-rapidez::button.primary v-else href="/login">
     <x-heroicon-o-heart class="h-5 w-5" />
-</x-rapidez::button>
+</x-rapidez::button.primary>
