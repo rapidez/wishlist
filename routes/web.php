ddb3e0bdbbd89b380bbc0f9789269559f6836746
indykoning <15870933+indykoning@users.noreply.github.com>
2023-07-04 10:28:48 +0200

    Use url helpers (#16)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::middleware('web')->group(function () {
-    Route::view('account/wishlist', 'rapidez::wishlist.account.wishlist');
+    Route::view('account/wishlist', 'rapidez::wishlist.account.wishlist')->name('account.wishlist');
 });
