bab2de5df57864de4325759e14e7a32a0f84607d
indy koning <indy@justbetter.nl>
2023-10-20 15:00:52 +0200

    Use authorization header from core if it is filled

--- resources/js/eventlisteners.js
+++ resources/js/eventlisteners.js
@@ -4,7 +4,7 @@ document.addEventListener('turbo:load', () => {
     window.app.$on('logged-in', async () => {
         let response = await axios.post(config.magento_url + '/graphql', {
             query: '{ customer { wishlists { id items_v2 { items { id product { sku id } } } } } }'
-        }, { headers: { Authorization: `Bearer ${localStorage.token}`, Store: config.store_code }})
+        }, { headers: { Authorization: window.magentoUser.defaults.headers.common['Authorization'], Store: config.store_code }})
 
         useLocalStorage('wishlist', response.data.data.customer.wishlists[0]).value = response.data.data.customer.wishlists[0];
         window.app.$emit('wishlist-changed')
