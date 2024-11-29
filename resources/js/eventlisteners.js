0bb5ecb9c09fa7b2b55dc1bba695ec509c2458c5
indykoning <15870933+indykoning@users.noreply.github.com>
2024-11-29 14:51:26 +0100

    Listen to vue:loaded instead

--- resources/js/eventlisteners.js
+++ resources/js/eventlisteners.js
@@ -1,6 +1,6 @@
 import { useLocalStorage } from "@vueuse/core";
 
-document.addEventListener('turbo:load', () => {
+document.addEventListener('vue:loaded', () => {
     window.app.$on('logged-in', async () => {
         let response = await window.magentoGraphQL(
             '{ customer { wishlists { id items_v2 { items { id product { sku id } } } } } }'
